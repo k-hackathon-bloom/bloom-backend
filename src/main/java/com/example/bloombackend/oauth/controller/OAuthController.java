b5c3c8c2290089e33a4da8f53182742ad3a77399
onegqueen <rkdwldnjs30@naver.com>
2024-09-09 18:22:19 +0900

    chore: conflict 해결

--- src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
+++ src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
@@ -3,12 +3,12 @@ package com.example.bloombackend.oauth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bloombackend.oauth.controller.dto.request.KakaoLoginRequest;
+import com.example.bloombackend.oauth.controller.dto.response.LoginResponse;
 import com.example.bloombackend.oauth.service.OAuthLoginService;
 
 @RestController
@@ -21,7 +21,7 @@ public class OAuthController {
 		this.oAuthLoginService = oAuthLoginService;
 	}
 
-	@PostMapping("/kakao")
+	@GetMapping("/kakao")
 	public ResponseEntity<LoginResponse> loginKakao(@RequestBody KakaoLoginRequest request) {
 		return ResponseEntity.ok(oAuthLoginService.login(request.authorizationCode()));
 	}
