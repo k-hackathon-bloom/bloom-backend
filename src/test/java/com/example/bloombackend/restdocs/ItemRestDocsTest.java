b39ff5bfb5f09767b01fcf6e5b5296a1e4c287b2
onegqueen <rkdwldnjs30@naver.com>
2024-12-18 21:45:44 +0900

    docs: 유저의 인벤토리 조회 api restDocs 작성 ref:#53

--- src/test/java/com/example/bloombackend/restdocs/ItemRestDocsTest.java
+++ src/test/java/com/example/bloombackend/restdocs/ItemRestDocsTest.java
@@ -22,9 +22,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.bloombackend.global.config.JwtTokenProvider;
+import com.example.bloombackend.item.entity.UserItemEntity;
 import com.example.bloombackend.item.entity.items.SeedEntity;
 import com.example.bloombackend.item.repository.ItemRepository;
 import com.example.bloombackend.item.repository.SeedRepository;
+import com.example.bloombackend.item.repository.UserItemRepository;
+import com.example.bloombackend.item.service.ItemService;
 import com.example.bloombackend.oauth.OAuthProvider;
 import com.example.bloombackend.user.entity.UserEntity;
 import com.example.bloombackend.user.repository.UserRepository;
@@ -44,10 +47,16 @@ public class ItemRestDocsTest {
 	private ItemRepository itemRepository;
 
 	@Autowired
+	private ItemService itemService;
+
+	@Autowired
 	private SeedRepository seedRepository;
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private UserItemRepository userItemRepository;
 
 	@SpyBean
 	private JwtTokenProvider jwtTokenProvider;
@@ -59,7 +68,12 @@ public class ItemRestDocsTest {
 	private ObjectMapper objectMapper;
 
 	private SeedEntity rose;
+
 	private SeedEntity limitFlower;
+
+	private SeedEntity defaultFlower;
+
+	private UserItemEntity userItem;
 
 	@BeforeEach
 	void setUp() {
@@ -93,6 +107,23 @@ public class ItemRestDocsTest {
 				.price(300).build()
 		);
 
+		defaultFlower = seedRepository.save(
+			SeedEntity.builder()
+				.name("기본씨앗 씨앗")
+				.endDate(LocalDate.of(9999, 12, 31))
+				.isDefault(true)
+				.thumbnailImgUrl("썸네일이미지")
+				.smallIconUrl("./resources/static/flower-icons/freesia-small.svg")
+				.bigIconUrl("./resources/static/flower-icons/freesia.svg")
+				.price(300).build()
+		);
+
+		userItem = userItemRepository.save(
+			UserItemEntity.builder()
+				.user(testUser)
+				.item(limitFlower).build()
+		);
+
 	}
 
 	@Test
@@ -115,6 +146,37 @@ public class ItemRestDocsTest {
 						"판매 마감일 +" + "\n" +
 							"- 시즌 한정 아이템의 경우, 실제 마감일이 표시됩니다. +" + "\n" +
 							"- 지속 판매 아이템의 경우, \"9999-12-31\"로 고정됩니다.")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("API - 유저의 인벤토리 아이템 목록 조회")
+	void getUserItemsTest() throws Exception {
+		// when & then
+		// 테스트 실행
+		mockMvc.perform(get("/api/item/inventory")
+				.header("Authorization", mockToken)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andDo(document("api-item-test/get-user-items",
+				preprocessResponse(prettyPrint()),
+				responseFields(
+					fieldWithPath("defaultItems[]").description("유저가 가진 기본 아이템 목록"),
+					fieldWithPath("defaultItems[].id").description("아이템 ID"),
+					fieldWithPath("defaultItems[].name").description("아이템 이름"),
+					fieldWithPath("defaultItems[].price").description("아이템 가격"),
+					fieldWithPath("defaultItems[].thumbnailUrl").description("아이템 썸네일 이미지 URL"),
+					fieldWithPath("defaultItems[].type").description("아이템 타입"),
+					fieldWithPath("defaultItems[].endDate").description("판매 종료일"),
+
+					fieldWithPath("purchasedItems[]").description("유저가 구매한 아이템 목록"),
+					fieldWithPath("purchasedItems[].id").description("아이템 ID"),
+					fieldWithPath("purchasedItems[].name").description("아이템 이름"),
+					fieldWithPath("purchasedItems[].price").description("아이템 가격"),
+					fieldWithPath("purchasedItems[].thumbnailUrl").description("아이템 썸네일 이미지 URL"),
+					fieldWithPath("purchasedItems[].type").description("아이템 타입"),
+					fieldWithPath("purchasedItems[].endDate").description("판매 종료일")
 				)
 			));
 	}
